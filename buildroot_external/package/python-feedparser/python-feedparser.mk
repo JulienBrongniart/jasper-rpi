9b7be3b6fe3b0b19835a23b744c918dbb9b1d4c5
Julien Brongniart <jbt@myfox.fr>
2016-09-20 00:04:20 +0200

    Update python packages with support/scripts/scanpypi tool

--- buildroot_external/package/python-feedparser/python-feedparser.mk
+++ buildroot_external/package/python-feedparser/python-feedparser.mk
@@ -5,8 +5,10 @@
 ################################################################################
 
 PYTHON_FEEDPARSER_VERSION = 5.2.1
-PYTHON_FEEDPARSER_SITE = $(call github,kurtmckee,feedparser,$(PYTHON_FEEDPARSER_VERSION))
-PYTHON_FEEDPARSER_SETUP_TYPE = distutils
+PYTHON_FEEDPARSER_SOURCE = feedparser-$(PYTHON_FEEDPARSER_VERSION).tar.bz2
+PYTHON_FEEDPARSER_SITE = https://pypi.python.org/packages/91/d8/7d37fec71ff7c9dbcdd80d2b48bcdd86d6af502156fc93846fb0102cb2c4
+PYTHON_FEEDPARSER_SETUP_TYPE = setuptools
+PYTHON_FEEDPARSER_LICENSE = OSI Approved
 PYTHON_FEEDPARSER_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
--- buildroot_external/package/python-pyaudio/python-pyaudio.mk
+++ buildroot_external/package/python-pyaudio/python-pyaudio.mk
@@ -6,9 +6,8 @@
 
 PYTHON_PYAUDIO_VERSION = 0.2.9
 PYTHON_PYAUDIO_SOURCE = PyAudio-$(PYTHON_PYAUDIO_VERSION).tar.gz
-PYTHON_PYAUDIO_SITE = https://pypi.python.org/packages/source/P/PyAudio
-PYTHON_PYAUDIO_SETUP_TYPE = distutils
-PYTHON_PYAUDIO_LICENSE = Python software foundation license v2
-PYTHON_PYAUDIO_LICENSE_FILES = LICENSE
+PYTHON_PYAUDIO_SITE = https://pypi.python.org/packages/d0/dc/ffb9ce5e3f19bd289902915a9f68b7d199216065f8ea17d5b5e8e4ad86ee
+PYTHON_PYAUDIO_SETUP_TYPE = setuptools
+PYTHON_PYAUDIO_LICENSE = MIT
 
 $(eval $(python-package))
